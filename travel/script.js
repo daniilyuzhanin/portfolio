de730e1f5eaa7e18cfe56d6f104df952176f9a90
Daniil Yuzhanin <daniilyuzhanin@gmail.com>
2021-06-09 23:01:05 +0300

    feat: header done

--- travel/script.js
+++ travel/script.js
@@ -1,12 +1,33 @@
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let nav = document.querySelector('.navigation');
 
 window.onscroll = () => {
 	searchBtn.classList.remove('fa-times');
 	searchBar.classList.remove('active');
-}
+	loginForm.classList.remove('active');
+	menu.classList.remove('fa-times');
+	nav.classList.remove('active');
+};
 
 searchBtn.addEventListener('click', () => {
 	searchBtn.classList.toggle('fa-times');
 	searchBar.classList.toggle('active');
+});
+
+menu.addEventListener('click', () => {
+	menu.classList.toggle('fa-times');
+	nav.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () => {
+	loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () => {
+	loginForm.classList.remove('active');
 });
